088813abade614f8885513a326bfde80a5264489
Jerald Tonmoy Dias <59910969+Jerald-tonmoy-dias@users.noreply.github.com>
2022-03-03 18:07:28 +0600

    file added

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,56 @@
 
 
 
+        // var options = {
+        //     series: [100],
+        //     chart: {
+        //         type: 'donut',
+        //     },
+        //     legend: {
+        //         show: false
+        //     },
+        //     dataLabels: {
+        //         enabled: false
+        //     },
+        //     tooltip: {
+        //         enabled: false
+        //     },
+        //     // fill: {
+        //     //     colors: ['#F45b9f2']
+        //     // }
+        // };
+
+        // var chart = new ApexCharts(document.querySelector("#chart-ratings"), options);
+        // chart.render();
 
 
+        // var options2 = {
+        //     series: [100],
+        //     chart: {
+        //         type: 'donut',
+        //     },
+        //     legend: {
+        //         show: false
+        //     },
+        //     dataLabels: {
+        //         enabled: false
+        //     },
+        //     tooltip: {
+        //         enabled: false
+        //     },
+        //     // fill: {
+        //     //     colors: ['#F45b9f2']
+        //     // }
+        // };
 
+        // var chart2 = new ApexCharts(document.querySelector("#ratings-distribution"), options2);
+        // chart2.render();
+
+
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
 
 
 
@@ -66,146 +113,9 @@
             }
         });
 
-        //scroll height
-        $(window).bind('scroll', function () {
-            $('.header-section').removeClass('add-padding');
-            if ($(window).scrollTop() > 200) {
-                $('.header-section').addClass('fixed');
-                $('.header-info-wrapper').addClass('hide-nav');
-                $('.header-info-wrapper').parent().removeClass('my-4');
-                $('.header-input').addClass('show');
-                // console.log('crosed 200 px');
-            } else {
-                $('.header-section').removeClass('fixed');
-                $('.header-info-wrapper').removeClass('hide-nav');
-                $('.header-input').removeClass('show');
-                $('.header-info-wrapper').parent().addClass('my-4');
-            }
-        });
-
-        // header scroll nav effect
-        $('.header-input').click(function () {
-            $('.header-info-wrapper').removeClass('hide-nav');
-            $('.header-input').removeClass('show');
-            $('.header-section').addClass('add-padding');
-
-        });
-
-
         // nice-select
         $('select').niceSelect();
 
-
-
-        // input focus btn
-        $("#location").focus(function () {
-            $(this).parent().removeClass("hide");
-            $(this).parent().addClass("show");
-        }).blur(function () {
-            $(this).parent().removeClass("show");
-            $(this).parent().addClass("hide");
-        });
-
-        // date input
-        $("#check_in").focus(function () {
-            $(this).attr('type', 'date');
-
-        }).blur(function () {
-            $(this).attr('type', 'text');
-        });
-
-        $("#check_out").focus(function () {
-            $(this).attr('type', 'date');
-
-        }).blur(function () {
-            $(this).attr('type', 'text');
-        });
-
-        // add guest
-        $("#add_guest").click(function () {
-            $(this).parent().toggleClass("show");
-            $('.cancel-guest').toggleClass("show");
-        });
-
-        $('.cancel-guest').click(function () {
-            $("#add_guest").parent().removeClass('show');
-            $(this).removeClass('show');
-        });
-
-        // vdo popup activation
-        $('.popup-link').magnificPopup({
-            type: 'iframe',
-            // other options
-            iframe: {
-                markup: '<div class="mfp-iframe-scaler">' +
-                    '<div class="mfp-close"></div>' +
-                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                    '</div>',
-
-                patterns: {
-                    youtube: {
-                        index: 'youtube.com/',
-
-                        id: 'v=',
-
-                        src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-                    },
-                    vimeo: {
-                        index: 'vimeo.com/',
-                        id: '/',
-                        src: '//player.vimeo.com/video/%id%?autoplay=1'
-                    },
-                    gmaps: {
-                        index: '//maps.google.',
-                        src: '%id%&output=embed'
-                    }
-
-                },
-
-                srcAction: 'iframe_src',
-            }
-        });
-
-        // single page slider
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: "auto",
-            spaceBetween: 30,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            breakpoints: {
-                576: {
-                    slidesPerView: 1,
-                    spaceBetween: 30,
-                },
-                768: {
-                    spaceBetween: 30,
-                    slidesPerView: 2,
-                },
-            }
-        });
-
-        // sidebar navbar
-        const mobileScreen = window.matchMedia("(max-width: 990px )");
-        $(document).ready(function () {
-            $(".dashboard-nav-dropdown-toggle").click(function () {
-                $(this).closest(".dashboard-nav-dropdown")
-                    .toggleClass("show")
-                    .find(".dashboard-nav-dropdown")
-                    .removeClass("show");
-                $(this).parent()
-                    .siblings()
-                    .removeClass("show");
-            });
-            $(".menu-toggle").click(function () {
-                if (mobileScreen.matches) {
-                    $(".dashboard-nav").toggleClass("mobile-show");
-                } else {
-                    $(".dashboard").toggleClass("dashboard-compact");
-                }
-            });
-        });
 
         /* end point */
     });
